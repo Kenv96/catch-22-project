469bc6b1525b67c27c15673bd06d621dc7b0a03a
Kenv96 <kesaley93@gmail.com>
2022-11-12 15:57:53 -0500

    removed unnecessary code from flask05

--- 3155project/flask05.py
+++ 3155project/flask05.py
@@ -1,5 +1,3 @@
-# FLASK Tutorial 1 -- We show the bare bones code to get an app up and running
-
 # imports
 import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
@@ -110,13 +108,6 @@ def delete_note(note_id):
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=False)
-
-# To see the web page in your web browser, go to the url,
-#   http://127.0.0.1:5000/
-#   http://127.0.0.1:5000/index
-#   http://127.0.0.1:5000/notes
-#   http://127.0.0.1:5000/notes/2
-#   http://127.0.0.1:5000/notes/new
 
 # Note that we are running with "debug=True", so if you make changes and save it
 # the server will automatically update. This is great for development but is a
