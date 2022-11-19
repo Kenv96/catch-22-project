9d5e52bc189922deb1bdcfe20d793f7a6b57e71d
Andrew Schulz <schulzandrew2516@gmail.com>
2022-11-18 20:33:27 -0500

    updated paths

--- 3155project/flask05.py
+++ 3155project/flask05.py
@@ -67,8 +67,8 @@ def new_project():
         a_user = db.session.query(User).filter_by(email='user@uncc.edu').one()
         return render_template('new.html', user=a_user)
     
-@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
-def update_project(note_id):
+@app.route('/projects/edit/<project_id>', methods=['GET', 'POST'])
+def update_project(project_id):
     #check method used for request
     if request.method == 'POST':
         #get title data
@@ -92,13 +92,13 @@ def update_project(note_id):
 
         return render_template('new.html', note=my_note, user=a_user)
 
-@app.route('/notes/delete/<note_id>', methods=['POST'])
-def delete_project(note_id):
+@app.route('/projects/delete/<project_id>', methods=['POST'])
+def delete_project(project_id):
     #retrieve note from database
     my_note = db.session.query(Note).filter_by(id=note_id).one()
     db.session.delete(my_note)
     db.session.commit()
-    return redirect(url_for('get_notes'))
+    return redirect(url_for('get_projects'))
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'),port=int(os.getenv('PORT', 5000)),debug=False)
